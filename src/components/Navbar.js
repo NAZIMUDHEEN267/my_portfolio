22924a10da00ac3d1cf835d6413d6633e3b655c1
nazimudheen267 <nazimudheen267@gmail.com>
2022-06-12 01:53:26 +0530

    navbar finished

---
+++ src/components/Navbar.js
@@ -0,0 +1,88 @@
+
+import { GoThreeBars } from "react-icons/go"
+
+const Navbar = () => {
+
+    // nav bar links function
+    const links = (drop) => {
+        return (
+            <>
+                {["Home", "About", "Services", "Contact"].map((link, i) => {
+                    return <li className={(drop) ? `link-${i}` : "link"} ><a href={link}>{link}</a></li>
+                })}
+            </>
+        )
+    }
+
+    const dropMenu = () => {
+        const bar = document.querySelector(".drop__links");
+        bar.classList.toggle("animation");
+    }
+
+    return (
+        <header className="header">
+
+            {/* navbar */}
+            <nav className="nav">
+                <div className="nav__name">
+                    <a href="#home">
+                        <h1>Nazimudheen</h1>
+                    </a>
+                </div>
+
+                <ul className="nav__links">{links()}</ul>
+
+                {/* bar icon for show nav links */}
+                <div className="nav__bars">
+                    <GoThreeBars className="bar__icon" onClick={dropMenu} />
+                </div>
+            </nav>
+
+            {/* drop down nav links when user click bar icon it will show*/}
+            <ul className="drop__links">{links(true)}</ul>
+        </header>
+
+    )
+}
+
+export default Navbar;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
